9c8a12d9e272ac2cc019eda968936f5ab069caff
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-30 23:57:24 -0800

    quick sort algo

--- main.py
+++ main.py
@@ -62,3 +62,13 @@ def merge_sort(arr):
             k += 1
 
     return arr
+
+def quick_sort(arr):
+    """Sorts an array using Quick Sort."""
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
